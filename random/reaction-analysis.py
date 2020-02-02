d424c62efdfd51029a263a6ddff31ded2824c962
Nikituh <aare.undo@gmail.com>
2020-02-02 18:23:39 +0100

    facebook reaction analysis

--- random/reaction-analysis.py
+++ random/reaction-analysis.py
@@ -1,8 +1,10 @@
+# -*- coding: UTF-8 -*-
 
 import json
 import operator
 import sys
 
+# Message Model
 class Message:
 
 	def __init__(self):
@@ -19,16 +21,18 @@ class Message:
     	
 		if "reactions" in json:
 			message.reactions = Reaction.list_from_json(json["reactions"])
+		else:
+			message.reactions = []
 		return message
 
+# Reaction Model
 class Reaction:
 	def __init__(self):
 		pass
 
 	def from_json(json):
 		reaction = Reaction()
-		print(json);
-		reaction.reaction = json["reaction"];
+		reaction.reaction = json["reaction"].encode("unicode-escape")
 		reaction.actor = json["actor"]
 		return reaction
 
@@ -38,6 +42,7 @@ class Reaction:
 			reactions.append(Reaction.from_json(child))
 		return reactions
 
+# Read and parse messages from files
 messages = []
 
 for i in range(1, 18):
@@ -47,5 +52,58 @@ for i in range(1, 18):
 
 	for message_json in messages_json:
 		message = Message.from_json(message_json)
+		messages.append(message)
+
+# Group messages by sender, print top
+messages_by_sender = {}
+
+for message in messages:
+	if message.sender_name in messages_by_sender:
+		messages_by_sender[message.sender_name] += 1
+	else:
+		messages_by_sender[message.sender_name] = 1
+
+sorted_sender_count = sorted(messages_by_sender.items(), key=operator.itemgetter(1), reverse=True)
+
+# counter = 1
+# for key, value in sorted_sender_count:
+#     print(str(counter) + ". " + str(key) + ": " + str(value))
+#     counter += 1
+
+# Group reactions, print top
+reaction_map = {
+	b'\\xf0\\x9f\\x98\\x86': "😆",
+	b'\\xf0\\x9f\\x98\\x8d': "😍",
+	b'\\xf0\\x9f\\x91\\x8d': "👍",
+	b'\\xf0\\x9f\\x98\\xae': "😮",
+	b'\\xf0\\x9f\\x98\\xa2': "😢",
+	b'\\xf0\\x9f\\x98\\xa0': "😠",
+	b'\\xe2\\x9d\\xa4':       "❤",
+	b'\\xf0\\x9f\\x91\\x8e': "👎"
+}
+
+reactions = {}
+
+for message in messages:
+	for reaction in message.reactions:
+		if reaction.reaction in reactions:
+			reactions[reaction.reaction] += 1
+		else:
+			reactions[reaction.reaction] = 1
+
+sorted_reaction_count = sorted(reactions.items(), key=operator.itemgetter(1), reverse=True)
+
+counter = 1
+for key, value in sorted_reaction_count:
+	print(str(counter) + ". " + reaction_map[key] + ": " + str(value))
+	counter += 1
+
+
+
+
+
+
+
+
 
 
