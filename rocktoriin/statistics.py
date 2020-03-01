6db583f3d1f5b1329e5bbe296aa639bce1b9a0be
Nikituh <aare.undo@gmail.com>
2020-03-01 16:51:18 +0100

    spotify credentials to gitignore, rocktoriin question model

--- rocktoriin/statistics.py
+++ rocktoriin/statistics.py
@@ -8,31 +8,41 @@ wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0) 
 sheet.cell_value(0, 0) 
 
+class Question:
+	def __init__(self, artist, song):
+		self.artist = artist
+		self.song = song
+
+questions = []
+
 artists = {}
 songs = {}
 
 for i in range(sheet.nrows): 
 	artist = sheet.cell_value(i, 1)
 	song = sheet.cell_value(i, 2)
+	questions.append(Question(artist, song))
 
-	if artist in artists:
-		artists[artist] += 1
+for question in questions:
+
+	if question.artist in artists:
+		artists[question.artist] += 1
 	else:
-		artists[artist] = 1
+		artists[question.artist] = 1
 
-	if song in songs:
-		songs[song] += 1
+	if question.song in songs:
+		songs[question.song] += 1
 	else:
-		songs[song] = 1
+		songs[question.song] = 1
 
-# sorted_artist_count = sorted(artists.items(), key=operator.itemgetter(1), reverse=True)
+sorted_artist_count = sorted(artists.items(), key=operator.itemgetter(1), reverse=True)
 
-# counter = 1
-# for key, value in sorted_artist_count:
-#     print(str(counter) + ". " + str(key) + " (" + str(value) + ")")
-#     counter += 1
-#     if counter == 11:
-#     	break
+counter = 1
+for key, value in sorted_artist_count:
+    print(str(counter) + ". " + str(key) + " (" + str(value) + ")")
+    counter += 1
+    if counter == 11:
+    	break
 
 
 sorted_song_count = sorted(songs.items(), key=operator.itemgetter(1), reverse=True)
@@ -43,3 +53,8 @@ for key, value in sorted_song_count:
     counter += 1
     if counter == 11:
     	break
+
+
+
+
+
