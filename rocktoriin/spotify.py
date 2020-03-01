bbeac10552779d0b2e36af4c63e2db590c5fdbea
Nikituh <aare.undo@gmail.com>
2020-03-01 17:23:03 +0100

    basic spotify script

---
+++ rocktoriin/spotify.py
@@ -0,0 +1,10 @@
+# https://github.com/plamere/spotipy
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+results = sp.search(q='weezer', limit=20)
+for idx, track in enumerate(results['tracks']['items']):
+    print(idx, track['name'])
