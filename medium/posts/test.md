5cdee8814a492013e4f09660ae438b78c8049234
Nikituh <aare.undo@gmail.com>
2021-08-11 12:08:29 +0200

    post template; basic publish script

---
+++ medium/posts/test.md
@@ -0,0 +1,17 @@
+## Markdown Title
+
+### Three-hash-subtitle
+
+`single_code_block`
+
+#### Four-hash-subtitle
+
+```
+multi_line_code_block
+```
+
+##### Five-hash-subtitle
+
+_italics_
+
+*bold*
---
+++ medium/publish-draft.py
@@ -0,0 +1,23 @@
+
+import requests
+import conf
+from pathlib import Path
+
+post = Path("posts/test.md").read_text()
+
+data = {
+    "title": "<add-clever-title-for-preview",
+    "contentFormat": "markdown",
+    "content": post,
+    "tags": ["development", "design"],
+    "publishStatus": "draft"
+}
+
+response = requests.post(
+    url= f"{conf.ENDPOINT}/users/{conf.USER_ID}/posts",
+    headers=conf.HEADER,
+    data=data,
+    params={"accessToken" : conf.INTEGRATION_TOKEN},
+)
+
+print(response.json())
