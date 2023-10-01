3dce0eb1a0305790f38ee703684221887929575b
Aare Undo <aare.undo@gmail.com>
2023-10-01 13:20:54 +0300

    remaining word alignment fix

--- wordle/wordle-helper/src/views/RemainingWordGallery.tsx
+++ wordle/wordle-helper/src/views/RemainingWordGallery.tsx
@@ -6,11 +6,19 @@ export default class RemainingWordGallery extends React.Component<any, any> {
     override render(): React.ReactNode {
         
         console.log("remaining words", this.props.words);
-        
+
         return (
-            <div style={{display: "flex", width: 200}}>
+            <div style={{display: "flex", flexWrap: "wrap", maxWidth: 400, paddingTop: 20}}>
                 {this.props.words.map((word: string) => {
-                    return <div>{word}</div>
+                    return <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            flexBasis: "20%",
+                            paddingBottom: 3
+                        }}
+                    >{word}</div>
                 })}
             </div>
         )
