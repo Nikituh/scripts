188a17bf032b78fa8d501c30e991ad6346921e04
Aare Undo <aare.undo@gmail.com>
2023-10-01 10:05:06 +0300

    wordle: mv calc. to service

--- wordle/wordle-helper/src/App.tsx
+++ wordle/wordle-helper/src/App.tsx
@@ -1,38 +1,24 @@
 import React from 'react';
+import WordCalculator from './utils/WordCalculator';
 
 export default class App extends React.Component<any, any> {
 
-	doesNotContain(list: string[], letter: string) {
-		return !list.includes(letter);
-	}
-
-	containsAtIndex(list: string[], letter: string, index: number): boolean {
-		return list[index] === letter;
-	}
-
-	containsButNotAtIndex(list: string[], letter: string, index: number): boolean {
-		return list.includes(letter) && list[index] !== letter;
-	}
-
 	override async componentDidMount(): Promise<void> {
-		const response = await fetch("data/et_5words.txt");
-		const text = await response.text();
-		const words = text.split("\n")
+
+		await WordCalculator.initialize();
 
 		const filtered: string[] = [];
 
-		words.forEach((word: string) => {
+		WordCalculator.words.forEach((word: string) => {
 			const letters = word.split("");
 			if (
-				this.containsAtIndex(letters, "a", 0) &&
-				this.containsAtIndex(letters, "e", 3) &&
-				this.containsButNotAtIndex(letters, "e", 1) &&
-				this.containsButNotAtIndex(letters, "l", 1) &&
-				this.doesNotContain(letters, "r") &&
-				this.doesNotContain(letters, "u") &&
-				this.doesNotContain(letters, "s") &&
-				this.doesNotContain(letters, "t") &&
-				this.doesNotContain(letters, "g")
+				WordCalculator.containsAtIndex(letters, "k", 2) &&
+				WordCalculator.containsAtIndex(letters, "t", 4) &&
+				WordCalculator.containsButNotAtIndex(letters, "k", 0) &&
+				WordCalculator.containsButNotAtIndex(letters, "t", 3) &&
+				WordCalculator.doesNotContain(letters, "o") &&
+				WordCalculator.doesNotContain(letters, "h") &&
+				WordCalculator.doesNotContain(letters, "u")
 			) {
 				filtered.push(word);
 			}
